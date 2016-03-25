531385129dc0118851f86a864152e46f770daf43
Tatum Szymczak <tatum@ashlandstudios.com>
2016-03-24 23:54:25 -0500

    add debugging code

--- articles/handler.js
+++ articles/handler.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports.handler = function(event, context) {
+  console.log('\n\n', {eventId: event.id, method: event.httpMethod}, '\n\n');
   return context.done(null, {
     message: 'Go Serverless! Your Lambda function executed successfully!'
   });
