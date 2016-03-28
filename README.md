bb4c332ce46d1b3531b54ffee8ec2e4efceeb5f7
Tatum Szymczak <tatum@ashlandstudios.com>
2016-03-28 08:21:11 -0500

    add params

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## HEADER
+x-api-key
+Ked45GFtvI6Ai3AC4It0Z9mfaCmhVJr53dvwmyP6
--- functions/articles/lib/action.js
+++ functions/articles/lib/action.js
@@ -5,6 +5,6 @@ var Promise = require("bluebird");
 module.exports.GET = function(event, context){
   return new Promise(function (resolve, reject){
     if (false) reject("ERROR: Im a rejected Promise")
-    else resolve("Im a returned Promise. Yay!");
+    else resolve("Im a returned Promise. Yay!" + '\n\n' + JSON.stringify(event) + '\n\n' + JSON.stringify(context));
   });
 }
